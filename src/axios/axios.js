4fb7d40102f6657c26b08e3f115617a625e23e6f
MohdSadab <sadabkhan14198@gmail.com>
2020-03-06 15:59:43 +0530

    comment added

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let axiosInstance = axios.create({
-  baseURL: " http://192.168.0.108/user"
+  baseURL: " http://192.168.0.108/user" //change this to your own base url
   /* other custom settings */
 });
 export default axiosInstance;
